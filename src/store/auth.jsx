ff74766d63b71603a4ddc2a29251347be2bc1e25
NO ONE <sohaibikram249@gmail.com>
2024-09-19 17:51:33 +0500

    update

---
+++ src/store/auth.jsx
@@ -0,0 +1,96 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState(null);
+  const [services, setServices] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const authorizationToken = `Bearer ${token}`;
+  const API = import.meta.env.VITE_SOLO_URL;
+  console.log(API);  
+
+  const storeTokenInLS = (serverToken) => {
+    setToken(serverToken);
+    localStorage.setItem("token", serverToken);
+  };
+
+  const isloggedIn = !!token;
+
+  const LogoutUser = () => {
+    setToken("");
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  const userAuthentication = async () => {
+    if (!token) return;
+
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${API}/api/auth/user`, {
+        method: "GET",
+        headers: {
+          Authorization: authorizationToken,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data.userData);
+        setIsLoading(false);
+      } else {
+        LogoutUser();
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log("Error fetching user data:", error);
+    }
+  };
+
+  const getServices = async () => {
+    try {
+      const response = await fetch(`${API}/api/data/service`, {
+        method: "GET",
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setServices(data);
+      }
+    } catch (error) {
+      console.log("Error fetching services:", error);
+    }
+  };
+
+  useEffect(() => {
+    userAuthentication();
+    getServices();
+  }, [token]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isloggedIn,
+        storeTokenInLS,
+        LogoutUser,
+        user,
+        services,
+        authorizationToken,
+        isLoading,
+        API,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const authContextValue = useContext(AuthContext);
+  if (!authContextValue) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return authContextValue;
+};
