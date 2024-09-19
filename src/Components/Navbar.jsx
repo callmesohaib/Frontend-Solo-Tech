9c98b10401eb820593b71f3120d8cefb6718cffa
NO ONE <sohaibikram249@gmail.com>
2024-09-19 22:05:22 +0500

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import { useAuth } from "../store/auth";
+import { useState } from "react";
 
 export const Navbar = () => {
-  const { user, isloggedIn} = useAuth();
+  const { user, isloggedIn } = useAuth();
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <>
@@ -14,37 +16,100 @@ export const Navbar = () => {
           </div>
           <nav>
             <ul>
-              <li>
-                <NavLink to="/"> Home</NavLink>
-              </li>
-              {user?.is_Admin && (
+              <div className="desktopMenu">
                 <li>
-                  <NavLink to="/admin">Admin</NavLink>
+                  <NavLink to="/"> Home</NavLink>
                 </li>
-              )}
-              <li>
-                <NavLink to="/about"> About</NavLink>
-              </li>
-              <li>
-                <NavLink to="/service"> Service</NavLink>
-              </li>
-              <li>
-                <NavLink to="/contact"> Contact</NavLink>
-              </li>
-              {isloggedIn ? (
+                {user?.is_Admin && (
+                  <li>
+                    <NavLink to="/admin">Admin</NavLink>
+                  </li>
+                )}
                 <li>
-                  <NavLink to="/logout"> Logout</NavLink>
+                  <NavLink to="/about"> About</NavLink>
                 </li>
-              ) : (
-                <>
+                <li>
+                  <NavLink to="/service"> Service</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact"> Contact</NavLink>
+                </li>
+                {isloggedIn ? (
                   <li>
-                    <NavLink to="/register"> Register</NavLink>
+                    <NavLink to="/logout"> Logout</NavLink>
                   </li>
+                ) : (
+                  <>
+                    <li>
+                      <NavLink to="/register"> Register</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/login"> Login</NavLink>
+                    </li>
+                  </>
+                )}{" "}
+              </div>
+              <li>
+                <i
+                  class="bx bx-menu-alt-right mobMenu"
+                  onClick={() => setShowMenu(!showMenu)}
+                ></i>
+              </li>
+              <div
+                className="navMenu"
+                style={{ display: showMenu ? "flex" : "none" }}
+              >
+                <li>
+                  <NavLink to="/" onClick={() => setShowMenu(false)}>
+                    Home
+                  </NavLink>
+                </li>
+                {user?.is_Admin && (
                   <li>
-                    <NavLink to="/login"> Login</NavLink>
+                    <NavLink to="/admin" onClick={() => setShowMenu(false)}>
+                      Admin
+                    </NavLink>
                   </li>
-                </>
-              )}
+                )}
+                <li>
+                  <NavLink to="/about" onClick={() => setShowMenu(false)}>
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/service" onClick={() => setShowMenu(false)}>
+                    Service
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact" onClick={() => setShowMenu(false)}>
+                    Contact
+                  </NavLink>
+                </li>
+                {isloggedIn ? (
+                  <li>
+                    <NavLink to="/logout" onClick={() => setShowMenu(false)}>
+                      Logout
+                    </NavLink>
+                  </li>
+                ) : (
+                  <>
+                    <li>
+                      <NavLink
+                        to="/register"
+                        onClick={() => setShowMenu(false)}
+                      >
+                        Register
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/login" onClick={() => setShowMenu(false)}>
+                        Login
+                      </NavLink>
+                    </li>
+                  </>
+                )}
+              </div>
             </ul>
           </nav>
         </div>
